a7fd11c6e8d069d444c7530fa996ef67a7bdabfa
alexmro <in@alexm.ro>
2024-02-02 20:39:15 +0100

    Set key for running the scene
    Enter interactive mode with the P key

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from ui import editor, gizmo
+import bpy
+
+from .ui import editor, gizmo
 
 bl_info = {
     "name": "Build native 3D applications",
@@ -6,15 +8,28 @@ bl_info = {
     "category": "Engine",
 }
 
+addon_keymaps = []
+
 
 def register():
     editor.register()
     gizmo.register()
 
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name='Window', region_type='WINDOW', space_type='EMPTY')
+        kmi = km.keymap_items.new(gizmo.MotorOperator.bl_idname, type='P', value='PRESS')
+        addon_keymaps.append((km, kmi))
+
 
 def unregister():
     editor.unregister()
     gizmo.unregister()
+
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
 
 
 if __name__ == "__main__":
